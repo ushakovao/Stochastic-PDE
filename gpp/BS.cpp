aa410adc8fc63976e92568ce5e63444e2a38e67d
ushakova <ushakova.oxana@gmail.com>
2016-07-05 15:17:22 +0200

    Add presentation in beamer(to finish), BS.cpp finishes, BS.geo adapted to Strikes, BS.cfg updated

--- gpp/BS.cpp
+++ gpp/BS.cpp
@@ -13,62 +13,33 @@ int main(int argc, char* argv[])
 
   // create mesh and exporting it (ADD GEO IN PROJECT FOLDER!!! )
 	
-  int Lx = doption(_name="Lx");
-  int Ly = doption(_name="Ly");
+  auto mesh = loadMesh( _mesh=new Mesh<Simplex<DIM>>,
+                          _filename="square.geo" );
 
-	mesh_ptrtype mesh = createGMSHMesh(_mesh = new mesh_type,
-																		_desc = createBSGeo( meshsize, Lx, Ly),
-																		_update=MESH_UPDATE_FACES | MESH_UPDATE_EDGES);
 
+	//Mesh Adaptation TODO
 	int adapt_iter = 0;
 	int max_iter = 10;
-
-	//Mesh Adaptation
-/*
-	MeshAdapt mash_adaption;
-	do{
-		 space_ptrtype Xh = space_type::New( mesh );
-        u = Xh->element();
-        v = Xh->element();
-
-
-*/
-
-
-/*
-  auto mesh = unitSquare();
-  auto exp=exporter(_mesh=mesh);
-  e->addRegions();
-  e->save();
-*/
-
-
-	
-
 
 
 	//determine space
   auto Xh = Pch<1>(mesh);
 	
-	//initialisation of forms
-	auto a  = form2( _trial=Xh,_test=Xh);
-	auto l = form1(_test = Xh);
-
+	//initialisation of elements
   auto u = Xh->element("u");
+	auto uold=Xh->element("u");
   auto v = Xh->element("v");
-	
+	auto vold = Xh ->element("v");
 
   // parameters
-  double T=doption(_name="T");
+  double T=doption(_name="T");//Time to maturity
   int K= doption(_name="K");// Strike price
-
-  double r=doption(_name="r") //risk-free rate
-  double sigmax = doption(_name="sigmax");
-	double sigmay = doption(_name="sigmay");
-	double rho = doption(_name="rho");
-	double dt =T/Nmax;
-  double mu = doption(_name="mu"); //drift rate 
-  
+  double r=doption(_name="r"); //Risk-free rate
+  double sigmax = doption(_name="sigmax");//Volatility for optionA
+	double sigmay = doption(_name="sigmay");//Volaility for optionB
+	double rho = doption(_name="rho");//Correlation 
+  double mu = doption(_name="mu"); //Drift rate 
+  double dt =doption(_name="dt");
 
   double alpha = doption(_name="alpha");
   double beta = doption(_name="beta");
@@ -78,21 +49,29 @@ int main(int argc, char* argv[])
   int Nmax = doption(_name="Nmax");
   double scale=doption(_name="scale");
   
-  auto u0 = max(K-x, 0); 
-
-
-
-	auto l = form1(_test=Xh);
-	l = integrate (_range = elements(mesh), _expr= (idt(x)*id(x)*(r+1/dt));
-
-	auto a  =form2(_trial=Xh, _test=Xh);
-	a = integrate (_range=elements(mesh),_expr = ((sigma*Px()/2)^2)*dx(u)*dx(v) + Px()*dx(u)*id(v)*((sigma^2)-r)+r*idt(u)*id(v));
-
-	a+=on(_range=boundaryfaces(mesh), _rhs=l, _element=u, _expr=cst(0.));
-
-
+  auto u0 = max(K-max(Px(),Py()), 0); 
+	
 	auto xvel = -Px()*r + Px()*sigmax^2+Px()*rho*sigmax*sigmay/2;
 	auto yvel = -Py()*r + Py()*sigmay^2+Py()*rho*sigmax*sigmay/2;
 
 
+//Introduction of forms
+	auto l = form1(_test=Xh);
+	auto a = form2(_trial=Xh, _test=Xh);
 
+//iterating
+	for (int i=0, i<0.5/dt; i++){
+		l = u0;
+	
+		a =  integrate (_range=elements(mesh),_expr = ((idt(u)*id(v)*(r+(1/dt))) + 
+																									dx(u)*dx(v) *(sigmax*Px())^2)/2+ 
+																									dy(u)*dy(v)*(sigmay*Py())^2)/2 + 
+																									(dy(u)*dx(v) + dx(u)*dy(v))*rho*sigmax*sigmay*Px()*Py()/2) - 
+																									((gradt(u)*vec(xvel, yvel))*id(v)/dt;
+		a+=on(_range=markedfaces(mesh,"out"), _rhs=l1, _element=u, _expr=cst(0.) );
+
+		a.solve( _solution=u, _rhs=l, _name="BS" );
+
+		uold = u;
+	}
+}
